f526ebbc18b070fb953f41a17a27151a10df3868
mayur_d_developer <dmayur57@gmail.com>
2018-03-30 21:54:00 +0530

    initial commit

---
+++ src/assets/jss/baseStyle.jsx
@@ -0,0 +1,46 @@
+const drawerWidth = 240;
+
+const baseStyle = theme => ({
+  root: {
+    flexGrow: 1,
+    zIndex: 1,
+    overflow: "hidden",
+    position: "relative",
+    display: "flex",
+    width: "100%",
+    height: "100%"
+  },
+  appBar: {
+    position: "absolute",
+    marginLeft: drawerWidth,
+    zIndex: theme.zIndex.drawer + 1
+    
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  navIconHide: {
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+    [theme.breakpoints.up("md")]: {
+      position: "relative"
+    }
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3
+  },
+  activeItem: {
+    backgroundColor: theme.palette.primary[100]
+  },
+  item: {}
+});
+
+export default baseStyle;
