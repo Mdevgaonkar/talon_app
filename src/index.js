a1d2a8839a5c322793babe5c7c3228495a0e9fac
mayur_d_developer <dmayur57@gmail.com>
2018-05-22 05:28:00 +0530

    added css styling for entire theme

--- src/index.js
+++ src/index.js
@@ -2,19 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
-
+import "./assets/css/base.css";
 import indexRoutes from "./routes/index.jsx";
 
-import registerServiceWorker from './registerServiceWorker';
+import './assets/css/base.css';
+import registerServiceWorker from "./registerServiceWorker";
 
 var hist = createBrowserHistory();
 
 ReactDOM.render(
   <Router history={hist}>
-    
     <Switch>
+      
       {indexRoutes.map((prop, key) => {
-        return <Route exact={prop.exact} path={prop.path} key={key} component={prop.component}/>
+        return (
+          <Route
+            exact={prop.exact}
+            path={prop.path}
+            key={key}
+            component={prop.component}
+          />
+        );
         // <Route  path="/inbox"  component={Inbox} />
       })}
     </Switch>
