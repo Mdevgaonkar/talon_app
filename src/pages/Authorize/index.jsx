bd1d5ae775789d0a2b789fbe6003caca03f438ff
mayur_d_developer <dmayur57@gmail.com>
2018-05-21 06:48:00 +0530

    added legacy login to auth page

--- src/pages/Authorize/index.jsx
+++ src/pages/Authorize/index.jsx
@@ -4,11 +4,16 @@ import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import authStyle from "./authorizeStyles";
-import { Typography, Card, Icon, IconButton } from "@material-ui/core";
+import { Typography, Card, Icon, IconButton, Button } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
 
 const image =
   "https://cdn.pixabay.com/photo/2017/08/08/15/43/nature-2611771_960_720.jpg";
 class Authorize extends React.Component {
+  state = {
+    valid_pass: true,
+    valid_email: true
+  };
   render() {
     const { classes, theme } = this.props;
     return (
@@ -32,7 +37,6 @@ class Authorize extends React.Component {
                 justify="center"
               >
                 <Grid item>
-                  {/* <Card className={classes.content}> */}
                   <Paper
                     className={classes.content}
                     style={{
@@ -114,8 +118,61 @@ class Authorize extends React.Component {
                         color="inherit"
                         align="center"
                       >
-                        {/* Or */}
+                        Or
                       </Typography>
+
+                      {/* <div className={classes.formMargin}> */}
+                      <div>
+                        <Grid
+                          container
+                          spacing={16}
+                          // className={classes.demo}
+                          alignItems="baseline"
+                        >
+                          <Grid item>
+                            <Icon color="action">email</Icon>
+                          </Grid>
+
+                          <Grid item xs="10">
+                            <TextField
+                              label="Email"
+                              placeholder="john.doe@example.com"
+                              fullWidth
+                              error={!this.state.valid_email}
+                            />
+                          </Grid>
+                        </Grid>
+                      </div>
+                      <div>
+                        <Grid
+                          container
+                          spacing={16}
+                          // className={classes.demo}
+                          alignItems="baseline"
+                        >
+                          <Grid item>
+                            <Icon color="action">lock</Icon>
+                          </Grid>
+
+                          <Grid item xs="10">
+                            <TextField
+                              label="Password"
+                              type="password"
+                              fullWidth
+                              error={!this.state.valid_pass}
+                            />
+                          </Grid>
+                        </Grid>
+                      </div>
+                      <div className={classes.getStartedButton}>
+                        <Button
+                          variant="flat"
+                          color="primary"
+                          className={classes.button}
+                        >
+                          Get Started
+                        </Button>
+                      </div>
                     </div>
                   </Paper>
                 </Grid>
