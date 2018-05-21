a4d33564be41b9e12d8fda8676e63c83870c471f
mayur_d_developer <dmayur57@gmail.com>
2018-05-21 05:54:00 +0530

    formated Appbar

--- src/components/Appbar/index.jsx
+++ src/components/Appbar/index.jsx
@@ -1,47 +1,49 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-import AuthButtons from '../AuthButtons'
-import baseStyle from '../../assets/jss/baseStyle';
+import AuthButtons from "../AuthButtons";
+import baseStyle from "../../assets/jss/baseStyle";
 const styles = baseStyle;
 
 class Appbar extends React.Component {
   state = {
-    title : this.props.title,
-    auth:this.props.auth
-  }
-
-  
+    title: this.props.title,
+    auth: this.props.auth
+  };
 
   render() {
-    const { classes, handleDrawerToggle, auth} = this.props;
+    const { classes, handleDrawerToggle, auth } = this.props;
 
-    
     return (
-      <div >
+      <div>
         <AppBar className={classes.appBar}>
-            <Toolbar>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={handleDrawerToggle}
-                className={classes.navIconHide}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="title" color="inherit" noWrap className={classes.flex}>
-                {this.state.title}
-              </Typography>
-              {this.props.children}
-              <AuthButtons auth ={auth} />
-            </Toolbar>
-          </AppBar>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerToggle}
+              className={classes.navIconHide}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="title"
+              color="inherit"
+              noWrap
+              className={classes.flex}
+            >
+              {this.state.title}
+            </Typography>
+            {this.props.children}
+            <AuthButtons auth={auth} />
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
@@ -50,7 +52,7 @@ class Appbar extends React.Component {
 Appbar.propTypes = {
   classes: PropTypes.object.isRequired,
   title: PropTypes.string.isRequired,
-  auth:PropTypes.bool.isRequired,
+  auth: PropTypes.bool.isRequired
 };
 
 export default withStyles(styles)(Appbar);
