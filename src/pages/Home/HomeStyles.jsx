d31da23fcc643f780fb0152935d5bcb971b1e823
mayur_d_developer <dmayur57@gmail.com>
2018-05-25 04:20:00 +0530

    added demo card and styles in Home

--- src/pages/Home/HomeStyles.jsx
+++ src/pages/Home/HomeStyles.jsx
@@ -1,6 +1,52 @@
-import baseStyle from "../../assets/jss/baseStyle";
+import baseStyle, { imagesStyles } from "../../assets/jss/baseStyle";
 
 const HomeStyles = {
-    ...baseStyle
-}
-export default HomeStyles
+  ...baseStyle,
+  ...imagesStyles,
+  cardCategory: {
+    color: "#999999",
+    margin: "0",
+    fontSize: "14px",
+    marginTop: "0",
+    paddingTop: "10px",
+    marginBottom: "0"
+  },
+  cardCategoryWhite: {
+    color: "rgba(255,255,255,.62)",
+    margin: "0",
+    fontSize: "14px",
+    marginTop: "0",
+    marginBottom: "0"
+  },
+  cardTitle: {
+    color: "#3C4858",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    "& small": {
+      color: "#777",
+      fontSize: "65%",
+      fontWeight: "400",
+      lineHeight: "1"
+    }
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    "& small": {
+      color: "#777",
+      fontSize: "65%",
+      fontWeight: "400",
+      lineHeight: "1"
+    }
+  }
+};
+export default HomeStyles;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-
+// Material-ui components
 import Typography from "@material-ui/core/Typography";
+import { Button, Grid } from "@material-ui/core";
+
+// icons
+import InfoOutline from "@material-ui/icons/InfoOutline";
+import LocalOffer from "@material-ui/icons/LocalOffer";
+
+//Custom Components
+import Card from "../../components/Card/Card.jsx";
+import CardBody from "../../components/Card/CardBody.jsx";
+import CardHeader from "../../components/Card/CardHeader";
+import CardFooter from "../../components/Card/CardFooter";
+import CardIcon from "../../components/Card/CardIcon";
+import GridItem from "../../components/Grid/GridItem";
+
+// import GridContainer from "../../components/Grid/GridContainer";
 
 import Layout from "../Layout.jsx";
 
 import HomeStyles from "./HomeStyles.jsx";
-import { Button } from "@material-ui/core";
 
 class Home extends React.Component {
   state = {
@@ -16,14 +30,52 @@ class Home extends React.Component {
 
   render() {
     const { classes, theme } = this.props;
+    const image =
+      "https://cdn.pixabay.com/photo/2017/08/08/15/43/nature-2611771_960_720.jpg";
     return (
       <Layout
         drawerItemSelected="Dashboard"
         title="Dashboard"
         auth={this.state.auth}
       >
-        <Typography noWrap>{"Hear me... and rejoice"}</Typography>
-        <Button variant="flat">Get Started</Button>
+        <Grid>
+          <GridItem xs={12} sm={6} md={3}>
+            <Card>
+              <CardHeader color="danger" stats icon>
+                <CardIcon color="danger">
+                  <InfoOutline />
+                </CardIcon>
+                <p className={classes.cardCategory}>Fixed Issues</p>
+                <h3 className={classes.cardTitle}>75</h3>
+              </CardHeader>
+              <CardFooter stats>
+                <div className={classes.stats}>
+                  <LocalOffer />
+                  Tracked from Github
+                </div>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={6}>
+            <Card style={{ width: "20rem" }}>
+              <img
+                style={{ height: "180px", width: "100%", display: "block" }}
+                className={classes.imgCardTop}
+                src={image}
+                alt="Card-img-cap"
+              />
+              <CardBody>
+                <h4 className={classes.cardTitle}>Card title</h4>
+                <Typography>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Typography>
+
+                <Button color="primary">Do something</Button>
+              </CardBody>
+            </Card>
+          </GridItem>
+        </Grid>
       </Layout>
     );
   }
