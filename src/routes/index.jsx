324e75c2730b2ccaff95bb1c81b64f2b5c377226
mayur_d_developer <dmayur57@gmail.com>
2018-06-30 10:43:54 +0530

    added auth handler route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,13 @@
 import Home from "../pages/Home";
 import Inbox from "../pages/Inbox";
 import Authorize from "../pages/Authorize";
+import AuthHandler from "../pages/AuthHandler";
 
 var indexRoutes = [
-  { path: "/", name: "Dashboard", component: Authorize, exact: true },
+  { path: "/", name: "Login", component: Authorize, exact: true },
+  { path: "/authorize", name: "Authorizer", component: AuthHandler},
   { path: "/inbox", name: "Inbox", component: Inbox },
-  { path: "/home", name: "Login", component: Home }
+  { path: "/home", name: "Dashboard", component: Home }
 ];
 
 export default indexRoutes;
