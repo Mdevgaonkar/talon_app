8341aea13265e1b45dd387baa26da9d0d4170d71
mayur_d_developer <dmayur57@gmail.com>
2018-06-30 10:45:31 +0530

    updated getAuth and  added authorize methods

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,8 +1,34 @@
 import axios from 'axios';
 
 
+let baseUrl = 'http://localhost:5000';
+
 export const getAuth = async () => {
-    let url = "http://localhost:5000/";
+    let url = baseUrl+"/";
+    try {
+        const auth_obj = await axios(url, {
+            method: 'GET',
+            // mode: 'no-cors',
+            // headers: {
+            //   'Access-Control-Allow-Origin': '*',
+            //   'Content-Type': 'application/json',
+            // },
+            withCredentials: true,
+            credentials: 'include'
+        });
+        return auth_obj.data;
+        
+    } catch (error) {
+        console.log('ERROR', error);
+        return {
+            auth: "error",
+        };
+
+    }
+}
+
+export const authorize = async (code) => {
+    let url = baseUrl+"/authorize?code="+code;
     try {
         const auth_obj = await axios(url, {
             method: 'GET',
