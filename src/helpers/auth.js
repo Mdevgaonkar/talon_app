2074f3839e011c648ae816fface966f2f68912a0
mayur_d_developer <dmayur57@gmail.com>
2018-07-01 23:54:50 +0530

    added redirectToHOme and logout methods

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,34 +1,31 @@
+import React from "react";
 import axios from 'axios';
-
 
 let baseUrl = 'http://localhost:5000';
 
 export const getAuth = async () => {
-    let url = baseUrl+"/";
-    try {
-        const auth_obj = await axios(url, {
-            method: 'GET',
-            // mode: 'no-cors',
-            // headers: {
-            //   'Access-Control-Allow-Origin': '*',
-            //   'Content-Type': 'application/json',
-            // },
-            withCredentials: true,
-            credentials: 'include'
-        });
-        return auth_obj.data;
-        
-    } catch (error) {
-        console.log('ERROR', error);
-        return {
-            auth: "error",
-        };
-
-    }
+    let url = baseUrl + "/";
+    return await requestGET(url);
+    
 }
 
 export const authorize = async (code) => {
-    let url = baseUrl+"/authorize?code="+code;
+    let url = baseUrl + "/authorize?code=" + code;
+    return await requestGET(url);
+}
+
+export const logout = async (history) => {
+    let url = baseUrl + "/authorize/signout";
+    await requestGET(url);
+        history.replace({
+            pathname: "/",
+            state: {
+                auth: false
+            }
+        });
+}
+
+const requestGET = async (url) => {
     try {
         const auth_obj = await axios(url, {
             method: 'GET',
@@ -41,12 +38,27 @@ export const authorize = async (code) => {
             credentials: 'include'
         });
         return auth_obj.data;
-        
+
     } catch (error) {
         console.log('ERROR', error);
         return {
             auth: "error",
         };
-
     }
+}
+
+
+export const redirectToLogin = (history) => {
+    return (
+        <div>
+      {
+        history.push({
+          pathname: "/",
+          state: {
+            auth: false
+          }
+        })
+      }
+      </div>
+    )
 }
