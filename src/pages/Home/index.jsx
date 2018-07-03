b9e3294fde2e287c8b6d95a5c3ae021c07b36027
mayur_d_developer <dmayur57@gmail.com>
2018-07-04 01:27:25 +0530

    Seperated stat widget to a module

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,19 +4,13 @@ import { withStyles } from "@material-ui/core/styles";
 // Material-ui components
 // none
 
-// icons
-import InfoOutline from "@material-ui/icons/InfoOutline";
-import LocalOffer from "@material-ui/icons/LocalOffer";
-
 //Custom Components
 import { redirectToLogin, setAuth } from "../../helpers/auth";
-import Card from "../../components/Card/Card.jsx";
-
-import CardHeader from "../../components/Card/CardHeader";
-import CardFooter from "../../components/Card/CardFooter";
-import CardIcon from "../../components/Card/CardIcon";
 import GridItem from "../../components/Grid/GridItem";
 import GridContainer from "../../components/Grid/GridContainer";
+
+//modules
+import StatWidget from "../../modules/StatWidget";
 
 import Layout from "../Layout.jsx";
 
@@ -33,22 +27,10 @@ class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = { auth: setAuth(props) };
-    // this.setAuth();
   }
 
-  // setAuth = () => {
-  //   if (this.props.location.state !== undefined)
-  //     if ("auth" in this.props.location.state) {
-  //       // console.log('AUTH-1.5', this.props.location.state.auth);
-  //       this.state = { auth: this.props.location.state.auth };
-  //       // console.log('AUTH-1.6', this.state.auth);
-  //       return true;
-  //     }
-  //   return false;
-  // };
-
   render() {
-    const { classes, history } = this.props;
+    const { history } = this.props;
     const { auth } = this.state;
     // console.log('AUTH-3', auth);
 
@@ -65,21 +47,7 @@ class Home extends React.Component {
       >
         <GridContainer>
           <GridItem xs={12} sm={6} md={3}>
-            <Card>
-              <CardHeader color="danger" stats icon>
-                <CardIcon color="danger">
-                  <InfoOutline />
-                </CardIcon>
-                <p className={classes.cardCategory}>Fixed Issues</p>
-                <h3 className={classes.cardTitle}>75</h3>
-              </CardHeader>
-              <CardFooter stats>
-                <div className={classes.stats}>
-                  <LocalOffer />
-                  Tracked from Github
-                </div>
-              </CardFooter>
-            </Card>
+            <StatWidget />
           </GridItem>
         </GridContainer>
       </Layout>
